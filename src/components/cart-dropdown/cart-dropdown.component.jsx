2fb042ea059acd48e16a76c67b3f6f9cce01326d
Keith Gibson <keithwgibson74@gmail.com>
2023-02-21 14:57:46 +0000

    Added component and context to allow items to be added to cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,21 @@
-import './cart-dropdown.styles.scss'
-import Button from '../button/button.component'
+import { useContext } from "react"
+import "./cart-dropdown.styles.scss"
+import Button from "../button/button.component"
+import CartItem from "../cart-item/cart-item.component"
+import { CartContext } from "../../contexts/cart.context"
 
-const CartDropdown =() => {
-    return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
-                <Button>GO TO CHECKOUT</Button>
-            </div>
-        </div>
-    )
+const CartDropdown = () => {
+	const { cartItems } = useContext(CartContext)
+	return (
+		<div className="cart-dropdown-container">
+			<div className="cart-items">
+				{cartItems.map((item) => (
+					<CartItem key={item.id} cartItem={item} />
+				))}
+			</div>
+			<Button>GO TO CHECKOUT</Button>
+		</div>
+	)
 }
 
 export default CartDropdown
