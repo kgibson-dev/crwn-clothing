d73cf89f29667a70effbbf8399481646b3000854
Keith Gibson <keithwgibson74@gmail.com>
2023-02-21 14:11:18 +0000

    Added cart icon and navigation componets with related context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,12 +3,16 @@ import { Fragment, useContext } from "react"
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg"
 import "./navigation.styles.scss"
 import { UserContext } from "../../contexts/user.context"
+import CartIcon from "../../components/cart-icon/cart-icon.component"
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component"
+import { CartContext } from "../../contexts/cart.context"
 
 import { signOutUser } from "../../utils/firebase/firebase.utils"
 
 const Navigation = () => {
 	const { currentUser, setCurrentUser } = useContext(UserContext)
-	
+	const { isCartOpen} = useContext(CartContext)
+
 	return (
 		<Fragment>
 			<div className="navigation">
@@ -29,7 +33,9 @@ const Navigation = () => {
 							SIGN IN
 						</Link>
 					)}
+					<CartIcon />
 				</div>
+				{isCartOpen && <CartDropdown />}
 			</div>
 			<Outlet />
 		</Fragment>
