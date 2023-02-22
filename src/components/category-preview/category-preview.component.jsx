94d5988a91235b79bcade71260aeee41511b4f47
Keith Gibson <keithwgibson74@gmail.com>
2023-02-22 15:00:45 +0000

    Fixed category-preview component to rend correct amount of items

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -12,7 +12,7 @@ const CategoryPreview = ({ title, products }) => {
 			</h2>
 			<div className="preview">
 				{products
-					.filter((_, idx) => idx > 4)
+					.filter((_, idx) => idx < 4)
 					.map((product) => (
 						<ProductCard key={product.id} product={product} />
 					))}
